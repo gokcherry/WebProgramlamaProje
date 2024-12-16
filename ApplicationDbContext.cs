1e3f119185fb652068b398cc9a471c826879d4ff
Gökçe Çiçek Yağmur <121450523+gokcherry@users.noreply.github.com>
2024-12-16 18:48:02 +0300

    DbContext

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebProgramlamaProje.Models;
+
+namespace WebProgramlamaProje.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Calisanlar> Calisanlar { get; set; }
+        public DbSet<CalisanGelir> CalisanGelir { get; set; }
+        public DbSet<CalisanUzmanlik> CalisanUzmanlik { get; set; }
+        public DbSet<Musteriler> Musteriler { get; set; }
+        public DbSet<Randevular> Randevular { get; set; }
+        public DbSet<Salon> Salon { get; set; }
+        public DbSet<UzmanlikAlanlari> UzmanlikAlanlari { get; set; }
+    }
+}
